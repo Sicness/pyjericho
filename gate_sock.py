b3aa2ab798f70ae539c1ad6f03ab12610318ee9f
sicness <sicness@darklogic.ru>
2013-10-14 21:47:08 +0400

    Normal interapt for gate_sock.py

--- gate_sock.py
+++ gate_sock.py
@@ -5,6 +5,8 @@ import zmq
 import argparse
 from threading import Thread
 import socket
+import signal
+import sys
 
 __debug = False
 SOCK_PORT=10001
@@ -26,27 +28,38 @@ if args.tests:                  # --tests
     if __debug:
         print("DEBUG: Zmq replay port is changed on %i" % (REP_PORT))
 
+srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+srv.bind(("0.0.0.0", SOCK_PORT))
+srv.listen(1)
+
+def signal_handler(signal, frame):
+    srv.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
 # CONNECTION WITH THE QUEUE
 context = zmq.Context()
 req = context.socket(zmq.REQ)
 req.connect("tcp://127.0.0.1:%i" % (REP_PORT))
 
 # RESEND SOCK -> QUEUE
-srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-srv.bind(("0.0.0.0", SOCK_PORT))
-srv.listen(1)
 while True:
-    s, addr = srv.accept()
-    if __debug:
-        print("DEBUG: sock connection from", addr)
-    # TODO: despatch open socks in separate thread?
-    data = s.recv(1024)
-    if __debug:
-        print("DEBUG: sock recv: %s" % (data))
     try:
-        req.send(data)
-    except zmq.error.ZMQError as e:
-        print("ERROR: can't send to queue: %s" % (e))
-    # TODO: Dispatch replay
-    req.recv()
-    s.close()
+        s, addr = srv.accept()
+        if __debug:
+            print("DEBUG: sock connection from", addr)
+        # TODO: despatch open socks in separate thread?
+        data = s.recv(1024)
+        if __debug:
+            print("DEBUG: sock recv: %s" % (data))
+        try:
+            req.send(data)
+        except zmq.error.ZMQError as e:
+            print("ERROR: can't send to queue: %s" % (e))
+        # TODO: Dispatch replay
+        req.recv()
+        s.close()
+    except KeyboardInterrupt:
+        signal_handler("KeyboardInterrupt", "")
