6fba331284ec0fad80481aef196064e6beb92045
sicness <sicness@darklogic.ru>
2013-10-15 20:53:07 +0400

    Added IR codes to center

--- center.py
+++ center.py
@@ -9,6 +9,7 @@ import argparse
 from sound import Ultra
 
 QUEUE_PORT = 5000
+__debug = False
 
 def debug_print(text):
     if __debug:
@@ -37,6 +38,29 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
+IR_codes = dict()
+def init_IR_codes():
+    """ Bind functions on IR codes """
+    #IR_codes.update( {b'FF629D' : say_temp} )     # Say temperature status
+    #IR_codes.update( {b'FFA857' : volume_inc} )   # increase volume
+    #IR_codes.update( {b'FFE01F' : volume_dec} )   # reduce volume
+    #IR_codes.update( {b'FF906F' : toSecureMode} )       # Will be noBodyHome
+    IR_codes.update( {b'FFC23D' : ultra.switch} )       # On/off radio
+    IR_codes.update( {b'BF09C35C' : ultra.switch} )     # On/off radio (big)
+    #IR_codes.update( {b'8BE68656' : holeNightLightAuto} )
+    #IR_codes.update( {b'B21F28AE' : hole_night_light.setManualStateOff} )
+    #IR_codes.update( {b'A6B1096A' : hole_night_light.setManualStateOn} )
+    #IR_codes.update( {b'24014B0' : holeLightAuto} )
+    #IR_codes.update( {b'8FC212DB' : hole_light.setManualStateOff} )
+    #IR_codes.update( {b'7960556F' : hole_light.setManualStateOn} )
+    #IR_codes.update( {b'FF10EF' : holeNightLightAuto} )
+    #IR_codes.update( {b'FF38C7' : hole_night_light.setManualStateOff} )
+    #IR_codes.update( {b'FF5AA5' : hole_night_light.setManualStateOn} )
+    #IR_codes.update( {b'FF30CF' : holeLightAuto} )
+    #IR_codes.update( {b'FF18E7' : hole_light.setManualStateOff} )
+    #IR_codes.update( {b'FF7A85' : hole_light.setManualStateOn} )
+
+init_IR_codes()
 
 def dispatch_msg(data):
     try:
@@ -44,6 +68,9 @@ def dispatch_msg(data):
         if args[1] == 'radio':
             debug_print("run ultra")
             ultra.switch()
+        if args[1] == 'IR':
+            if args[2] in IR_codes:
+                IR_codes[args[2]]()
     except Exception  as e:
         print "ERROR: on parse msg: %\n%s" % (data, e)
 
